ed88269f862f22b6c77ddb130516d4bbe5fefe11
fkrdatabricks <fkrdatabricks@gmail.com>
2026-03-18 09:43:38 +0530

    Added doc string

--- multiplication.py
+++ multiplication.py
@@ -1,3 +1,9 @@
 # The is newly added function just for testing
 def multiply(num1, num2):
+    """This function takes two parameter as an input
+    and returns the result of their multiplicaiton
+    :param num1:
+    :param num2:
+    :return:
+    """
     return num1 * num2
